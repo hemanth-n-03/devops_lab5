4fd0d0bbc8e65e36fa1cfdce18efccc40a990e13
hemanth-n-03 <hemanthn1006@gmail.com>
2026-03-25 16:15:24 +0530

    Update app.java

--- app.java
+++ app.java
@@ -1,4 +1,4 @@
-public class App {
+public class app {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins and Docker!");
         System.out.println("Java Pipeline is running successfully.");
